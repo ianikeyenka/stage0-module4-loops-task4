353e9a77afa3a1d5bdb7935a07001bf51ad42859
Ilya Anikeyenka <20prorok02@gmail.com>
2023-03-05 16:52:55 +0300

    Solution

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+        int skipSum = 0;
+        int countSum = 0;
+        if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        } else if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        } else {
+            for (int i = 1; i <= lastInRow; i++) {
+                if (i <= numberToSkip) {
+                    skipSum += i;
+                } else {
+                    countSum += i;
+                }
+            }
+            System.out.println("skipped sum is " + skipSum + "\ncounted sum is " + countSum);
+        }
     }
 }
